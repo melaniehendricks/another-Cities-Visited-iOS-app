53d0a8d9856dba7ae36c69e446eb3a1b825f2ee1
Melanie Hendricks <melanie.r.hendricks@gmail.com>
2020-08-06 16:24:23 -0700

    commit

---
+++ Homework2/City+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  City+CoreDataClass.swift
+//  Homework2
+//
+//  Created by Melanie Hendricks on 8/6/20.
+//  Copyright © 2020 Melanie Hendricks. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class City: NSManagedObject {
+
+}
---
+++ Homework2/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Homework2
+//
+//  Created by Melanie Hendricks on 8/6/20.
+//  Copyright © 2020 Melanie Hendricks. All rights reserved.
+//
+
+import Foundation
+import CoreData
+public class Model{
+    
+}
